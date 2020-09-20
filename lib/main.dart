a22898489d069966a42a887897764e1614fa0894
Henry-Ning <henryhuning@gmail.com>
2020-09-20 04:55:11 -0700

    47. Introducing "if" Statements 7min

--- lib/main.dart
+++ lib/main.dart
@@ -17,31 +17,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    }
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions');
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['Max', 'Max', 'Max', 'Max'],
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -52,7 +58,8 @@ class _MyAppState extends State {
             Question(
               questions[_questionIndex]['questionText'],
             ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
               return Answer(_answerQuestion, answer);
             }).toList()
           ],
